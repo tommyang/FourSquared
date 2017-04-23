b1fe76d6f4e89a57e6194dbbc585a8d880bd06f0
henyu6 <henyu6@gmail.com>
2017-04-23 14:26:55 -0700

    added password prompt

--- routes/password.js
+++ routes/password.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('password', { title: 'Enter your password:' });
+  res.render('password', { title: 'Password', 
+                           pmpt: 'Enter your password:' });
 });
 
 module.exports = router;
