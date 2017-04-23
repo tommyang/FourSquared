57035703fc26e650c8229b2d590ba62d4e579440
Tian Yang <tiyang@ucsd.edu>
2017-04-23 15:27:16 -0700

    use node module system

--- test/test-foo.js
+++ test/test-foo.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const assert = require('chai').assert;
 
 describe('foo', function() {
-  const foo = require('../foo.js').foo;
+  const foo = require('../foo');
   it('should return true for input "baz"', function() {
     expect(foo("baz")).to.be.true;
   });
