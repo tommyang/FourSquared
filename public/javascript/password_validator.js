366cb72dc665b18048c0086c4e714ea8f5d8c103
Hung Nguyen <hg.ngyn@gmail.com>
2017-04-24 13:01:29 -0700

    isValidPassword implemented

--- public/javascript/password_validator.js
+++ public/javascript/password_validator.js
@@ -1,12 +1,45 @@
-function validatePassword() {
+function isValidPassword() {
+    var password = $("#password").val();
+    var passwordScheme = setPasswordScheme();
 
+    if ("length" in passwordScheme) {
+        if (password.length < passwordScheme["length"]) {
+            return false; 
+        }
+    }
+
+    if ("lowercase" in passwordScheme) {
+        if (password.toUpperCase() === password) {
+            return false;
+        }
+    }
+
+    if ("uppercase" in passwordScheme) {
+        if (password.toLowerCase() === password) {
+            return false;
+        }
+    }
+
+    if ("number" in passwordScheme) {
+        if (!/\d/.test(password)) {
+            return false;
+        }
+    }
+
+    if ("special" in passwordScheme) {
+        if (!/[!@#$%?]/.test(password)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 function setPasswordScheme() {
     var checkedBoxes = {};
 
     if ($("#lengthBox").is(":checked")) {
-        checkedBoxes["length"] = $("#lengthval").val();
+        checkedBoxes["length"] = Number($("#lengthval").val());
     }
 
     if ($("#lowercaseBox").is(":checked")) {
@@ -29,5 +62,5 @@ function setPasswordScheme() {
 }
 
 $("#validate").click(function() {
-    console.log(setPasswordScheme());
+    isValidPassword(); // do something visual with this?!
 });
