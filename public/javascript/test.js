53e0f381c375aca0a50026161b31fb49b9499c0b
david-cleary <david.cleary.1@ucdconnect.ie>
2017-05-03 14:45:49 -0700

    Testing documentation generation

---
+++ public/javascript/test.js
@@ -0,0 +1,11 @@
+/**
+ * Test function that returns the larger of 2 values
+ * @param {Number} a
+ * @param {Number} b
+ *
+ * @return {Number} The bigger value of a and b
+ */
+function test(a,b){
+	if a>b return a
+	else return b
+}
