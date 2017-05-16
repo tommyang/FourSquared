cdb24f5183d27e13bc3ad848ad688b7d64507170
Hung Nguyen <hg.ngyn@gmail.com>
2017-05-16 01:28:37 -0700

    add login test, comment out karma conf

---
+++ test/e2e-login.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.should();
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+const assert = chai.assert;
+const should = chai.should;
+const webdriver = require('selenium-webdriver'),
+By = webdriver.By,
+until = webdriver.until;
+const server = require('../bin/www').server;
+const driver = new webdriver.Builder().forBrowser('phantomjs').build();
+
+describe('Login page', function(){
+
+    beforeEach(function() {
+        this.timeout(10000);
+        return driver.get('http://localhost:3000/login');
+    });
+
+    it('should have email field', function() {
+        driver.findElements(By.id('email')).should.not.equal(0);        
+    });
+
+    it('should be able to type in email', function() {
+        var emailField = driver.findElement(By.id('email'));
+        emailField.sendKeys("asdf@asdf.com");
+        emailField.getAttribute("value");.should.equal("asdf@asdf.com");
+    });
+
+    it('should have password field', function() {
+        driver.findElements(By.id('password')).should.not.equal(0);
+    });
+});
