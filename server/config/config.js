6ebc2adbcc1ae3e9feec20aad3bf9584ef137c1f
Hung Nguyen <hg.ngyn@gmail.com>
2017-05-25 16:32:42 -0700

    add database link up

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/webstormtroopers',
+  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://test:JjMRaJoyaJ}nxg6+A/63@ds153501.mlab.com:53501/emissary' || 'mongodb://localhost:27017/webstormtroopers',
   port: 3000,
   secret: process.env.SECRET || 'mysecret'
 };
