164cfcad38a3229babb80889d92766e9a6ec8c5d
Tian Yang <tiyang@ucsd.edu>
2017-05-26 16:40:19 -0700

    bring back scripts/

---
+++ scripts/deploy-server.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+### Configuration ###
+
+APP_DIR=/home/foursquared/FourSquared
+RESTART_ARGS=foursquared
+
+### Automation steps ###
+
+set -x
+
+# Pull latest code
+cd $APP_DIR
+git pull
+
+# Install dependencies
+yarn install --production
+
+# Restart app
+pm2 gracefulReload $RESTART_ARGS
