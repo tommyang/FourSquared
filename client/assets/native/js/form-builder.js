dbf9933d0ff03ba3b1fe70363ef63a9897a987b7
henyu6 <henyu6@gmail.com>
2017-05-29 17:44:47 -0700

    client side form builder updated

---
+++ client/assets/native/js/form-builder.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    var companyData = JSON.parse(localStorage.getItem("currentCompany"));
+    var myCompanyId = companyData._id;
+    var curUser = JSON.parse(localStorage.getItem('currentUser'));
+
+    var forms = getForms();
+
+    $('#myform').submit(function(event) {
+        var d = grabElements();
+        console.log(d);
+        updateForm(d);
+        forms = getForms();
+
+    });
+
+    function getForms() {
+        var json;
+        $.ajax({
+            dataType: 'json',
+            type: 'GET',
+            data: $('#response').serialize(),
+            async: false,
+            url: '/api/formtemplates/company/'+myCompanyId,
+            success: function(response) {
+                json = response;
+                console.log(response);
+            }
+        });
+    }
+
+    function updateForm(obj) {
+        $.ajax({
+            dataType: 'json',
+            type: 'POST',
+            data: obj,
+            async: false,
+            url: '/api/formtemplates/',
+            success: function(response) {
+                forms.push(response);
+                console.log(response);
+            }
+        });
+    }
+
+    function grabElements() {
+        var form = {};
+       
+        var field = 0;
+
+        $("input:checkbox").each(function() {
+            var label = $("label[for='" + this.id + "']").text();
+            var checked = $(this).is(":checked");
+           
+            var str = "";
+            if(checked) 
+                str = "true_" + label;
+            else
+                str = "false_" + label;
+
+            console.log(str);
+            form[field] = str;
+            field = field + 1;
+        });
+        return field;
+    }
+
+});
