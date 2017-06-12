9437c3b8f9b1b93ab1291e7da4588150c1d53bae
henyu6 <henyu6@gmail.com>
2017-06-11 18:27:22 -0700

    remove added lines

--- server/app.js
+++ server/app.js
@@ -135,12 +135,6 @@ app.get('/index', function(req,res){
   res.sendFile(path.join(__dirname,'../client/assets/views/index.html'))
 });   
 
-app.get('/fbwebhook/', function(req, res) {
-    if (req.query['hub.verify_token'] === 'lolpassword') {
-         res.send(req.query['hub.challenge'])
-    } else 
-        res.send('Error, wrong token')
-});
 /*
  * Error Handler.
  */
